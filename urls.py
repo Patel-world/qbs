87cad4152048957275e50c9e64ffe9b4e6c52a50
Patel-world <62463564+Patel-world@users.noreply.github.com>
2021-07-28 23:47:04 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -11,9 +11,11 @@ sitemaps = {
 
 urlpatterns = [
      path('', views.story_list, name='story_list'),
+  
+     path('search/',views.search,name='search'),
      path('<slug:category_slug>', views.story_list, name='story_by_category'),
      path('<slug:slug>/', views.story_detail, name='story_detail'),
-     path('search/',views.search,name='search'),
+     
      path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
    # path('admin/', admin.site.urls),
