29c52f850c26bf07c3afd69dcb3d343ffbb8038b
Patel-world <62463564+Patel-world@users.noreply.github.com>
2021-07-28 19:51:35 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ app_name='story'
 urlpatterns = [
      path('', views.story_list, name='story_list'),
      path('<slug:category_slug>', views.story_list, name='story_by_category'),
-     path('<int:id>/', views.story_detail, name='story_detail'),
+     path('<slug:slug>/', views.story_detail, name='story_detail'),
      path('search/',views.search,name='search'),
 
    # path('admin/', admin.site.urls),
